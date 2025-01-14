6551b4650f84554d3a8e9cd1f1fe963ec681cd84
muguowei <123456789>
2025-01-14 13:53:28 +0800

    1

--- sql/update_v1.0.sql
+++ sql/update_v1.0.sql
@@ -1,2 +1,7 @@
 alter table tb_image
     add process_flag  varchar(255) not null default '5' comment '处理状态，1-解析中、2-解析失败、3-可用、4-上传失败、5-上传中';
+alter table tb_image
+    alter column create_time set default (CURRENT_TIMESTAMP);
+
+alter table tb_image
+    alter column update_time set default (CURRENT_TIMESTAMP);
