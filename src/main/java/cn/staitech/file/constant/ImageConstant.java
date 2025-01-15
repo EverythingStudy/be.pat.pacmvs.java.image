97d1e17d36a98bc054b0c4519eadb6fafeef3421
muguowei <123456789>
2025-01-15 17:06:27 +0800

    1

--- src/main/java/cn/staitech/file/constant/ImageConstant.java
+++ src/main/java/cn/staitech/file/constant/ImageConstant.java
@@ -18,8 +18,8 @@ public class ImageConstant {
     public static final String IMAGE_PROCESS_PARSING = "1";
     public static final String IMAGE_PROCESS_PARSE_SUCCESS = "3";
     public static final String IMAGE_PROCESS_PARSE_FAIL = "2";
-    //public static final String IMAGE_PROCESS_UPLOAD_FAIL = "0";
-    //public static final String IMAGE_PROCESS_UPLOADING = "5";
+    public static final String IMAGE_PROCESS_UPLOAD_FAIL = "0";
+    public static final String IMAGE_PROCESS_UPLOADING = "5";
     public static final Integer IMAGE_SOURCE_UPLOAD = 1;
     public static final Integer IMAGE_SOURCE_SERVER = 2;
 
