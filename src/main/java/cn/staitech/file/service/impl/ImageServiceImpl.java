5231821abf5445187cb3abba3dabbd86b8e3dc9f
muguowei <123456789>
2025-06-05 16:26:31 +0800

    refactor(file): 优化图像服务中的异常处理逻辑
    - 将直接抛出异常改为先记录日志，再抛出异常
    - 添加了对存在图像路径的日志记录，便于排查问题
    -通过注释暂时禁用了异常抛出，可能为了调试或后续处理

--- src/main/java/cn/staitech/file/service/impl/ImageServiceImpl.java
+++ src/main/java/cn/staitech/file/service/impl/ImageServiceImpl.java
@@ -86,13 +86,14 @@ public class ImageServiceImpl extends ServiceImpl<ImageMapper, Image> implements
         int exists = imageMapper.selectCount(Wrappers.<Image>lambdaQuery()
                 .in(Image::getImagePath, filePaths)
                 .eq(Image::getOrganizationId, vo.getOrganizationId()));
-        if (exists>0) {
+        if (exists > 0) {
             List<Image> existImages = imageMapper.selectList(Wrappers.<Image>lambdaQuery()
                     .in(Image::getImagePath, filePaths)
                     .eq(Image::getOrganizationId, vo.getOrganizationId()));
-            throw new DuplicateKeyException("服务器选片异常:["
+            log.warn("服务器选片异常:[{}]文件已经存在", existImages.stream().map(Image::getImagePath).collect(Collectors.joining(", ")));
+            /*throw new DuplicateKeyException("服务器选片异常:["
                     + existImages.stream().map(Image::getImagePath).collect(Collectors.joining(", "))
-                    + "]文件已经存在");
+                    + "]文件已经存在");*/
         }
 
         List<Image> images = new ArrayList<>();
