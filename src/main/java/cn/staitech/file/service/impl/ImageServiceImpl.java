7eb0770529f4f9c9a5d07e648324e0a2ed2e4917
muguowei <123456789>
2025-06-09 14:07:36 +0800

    fix(file): 优化图像上传逻辑
    - 移除已存在文件的处理，避免重复上传
    - 修改循环遍历的集合，提高处理效率

--- src/main/java/cn/staitech/file/service/impl/ImageServiceImpl.java
+++ src/main/java/cn/staitech/file/service/impl/ImageServiceImpl.java
@@ -90,6 +90,7 @@ public class ImageServiceImpl extends ServiceImpl<ImageMapper, Image> implements
             List<Image> existImages = imageMapper.selectList(Wrappers.<Image>lambdaQuery()
                     .in(Image::getImagePath, filePaths)
                     .eq(Image::getOrganizationId, vo.getOrganizationId()));
+            filePaths.removeAll(existImages.stream().map(Image::getImagePath).collect(Collectors.toList()));
             log.warn("服务器选片异常:[{}]文件已经存在", existImages.stream().map(Image::getImagePath).collect(Collectors.joining(", ")));
             /*throw new DuplicateKeyException("服务器选片异常:["
                     + existImages.stream().map(Image::getImagePath).collect(Collectors.joining(", "))
@@ -97,7 +98,7 @@ public class ImageServiceImpl extends ServiceImpl<ImageMapper, Image> implements
         }
 
         List<Image> images = new ArrayList<>();
-        for (String path : vo.getFileList()) {
+        for (String path : filePaths) {
             File file = new File(path);
             String imageName = file.getName();
             Image image = createImageFromPath(vo.getOrganizationId(), path, imageName);
